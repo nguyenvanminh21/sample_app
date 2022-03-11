284547b3aab0fd0efb20fe2958f3bd23242d800c
nguyenvanminh21 <nguyen.van.minh-c@sun-asterisk.com>
2022-03-11 09:20:37 +0700

    static_page

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 
 ruby "2.7.0"
 gem "bootsnap", require: false
+gem "bootstrap-sass", "3.4.1"
 gem "importmap-rails"
 gem "jbuilder"
 gem "mysql2", "~> 0.5"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :posts
+  get 'static_pages/home'
+  get 'static_pages/help'
+  root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  #root 'users#index'
+end
+
